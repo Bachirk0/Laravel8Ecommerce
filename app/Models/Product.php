8d03f6373d3feb337b54c9b8a990ea0b754ea0db
Bachirk0 <bachirk0@gmail.com>
2022-03-01 18:35:33 +0000

    new commit1

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,6 +16,7 @@ class Product extends Model
   return $this->belongsTo(Category::class,
   'category_id');
  }
+ 
  public function orderItems()
  {
      return $this->hasMany(OrderItem::class, 'product_id');
