9ecdf2fa12a0f5a415086ef8f381e7c3842a41a6
Bachirk0 <bachirk0@gmail.com>
2022-03-04 14:26:06 +0000

    nouveau

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -19,9 +19,9 @@ class HomeComponent extends Component
         $category= HomeCategory::find(1);
         
       
-        $no_of_products= $category->no_of_prooducts;
+        
         $sproducts= Product::where('sale_price','>',0)->inRandomOrder()->get()->take(8);
       
-        return view('livewire.home-component',['sliders'=>$sliders,'lproducts'=>$lproducts, 'no_of_products'=>$no_of_products, 'sproducts'=>$sproducts])->layout('layouts.base');
+        return view('livewire.home-component',['sliders'=>$sliders,'lproducts'=>$lproducts,  'sproducts'=>$sproducts])->layout('layouts.base');
     }
 }
