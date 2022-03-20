bbe47bb10a4733f7f8f8edc22656ca05378b785d
Bachirk0 <bachirk0@gmail.com>
2022-03-20 01:00:58 +0000

    nouveau

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -437,14 +437,14 @@
 		</div>
 	</footer>
 	
-	<script src="{{ asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
-	<script src="{{ asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
-	<script src="{{ asset('assets/js/bootstrap.min.js')}}"></script>
-	<script src="{{ asset('assets/js/jquery.flexslider.js')}}"></script>
-	<script src="{{ asset('assets/js/owl.carousel.min.js')}}"></script>
-	<script src="{{ asset('assets/js/jquery.countdown.min.js')}}"></script>
-	<script src="{{ asset('assets/js/jquery.sticky.js')}}"></script>
-	<script src="{{ asset('assets/js/functions.js')}}"></script>
+	<script src="{{secure_asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+	<script src="{{secure_asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+	<script src="{{secure_asset('assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{secure_asset('assets/js/jquery.flexslider.js')}}"></script>
+	<script src="{{secure_asset('assets/js/owl.carousel.min.js')}}"></script>
+	<script src="{{secure_asset('assets/js/jquery.countdown.min.js')}}"></script>
+	<script src="{{secure_asset('assets/js/jquery.sticky.js')}}"></script>
+	<script src="{{secure_asset('assets/js/functions.js')}}"></script>
 	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>	
